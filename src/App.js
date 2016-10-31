449ed5a0efbb249a0b5ce760732734a94a5b2399
Jehan <jehan@Jehans-MacBook.local>
2016-10-30 21:54:26 -0700

    linting seems to wrk

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar/>
-        <Button>Foo</Button>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
