d8bfd1f954a5fb9d935f92b1c12313105a573034
Jehan <jehan@Jehans-MacBook.local>
2016-10-30 23:00:00 -0700

    react bootstrap seems to work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <Button color="primary">Foooooooo</Button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
