43229cfba9ab5f8c581a6ef17de650d776f4c5d0
Benny Lichtner <bennlich@gmail.com>
2018-06-24 00:04:46 -0700

    remove hardcoded hostname. fixes #1

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  ubusUrl: 'http://172.30.0.1/ubus',
-  infogatherUrl: 'http://172.30.0.1/cgi-bin/infogather',
+  ubusUrl: '/ubus',
+  infogatherUrl: '/cgi-bin/infogather',
   username: 'admin'
 }
